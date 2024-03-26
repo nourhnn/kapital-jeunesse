fca918a1987202959f28b89cc29670f56a059951
= <noussaiba.dha@gmail.com>
2024-03-26 15:58:12 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kapital Jeunesse</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Niramit:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header class="header-accueil">
+        <div class="triangle-down"></div>
+        <nav class="nav-desktop">
+            <ul>
+                <li><a href="Home.html">Accueil</a></li>
+                <li><a href="About.html">A propos</a></li>
+                <li><a href="Programs.html">Nos actions</a></li>
+            </ul>
+            <!-- <img class="logo" src="img/logo-couleur.png" alt="Logo de l'association Kapital     Jeunesse"> -->
+            <ul>
+                <li><a href="Donate.html">Dons</a></li>
+                <li><a href="Location.html">Localisation</a></li>
+                <li><a href="Contact.html">Contact</a></li>
+            </ul>
+        </nav>
+        <img class="logo-nav" src="assets/img/logo-couleur.png" alt="Logo de l'association Kapital Jeunesse">
+  </header>
+</body>
+</html>
