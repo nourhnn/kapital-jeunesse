f1fdee2c4f007cf763d37ce1059122d112fc5420
= <noussaiba.dha@gmail.com>
2024-03-28 01:41:25 +0100

    accueil v-1

--- index.php
+++ index.php
@@ -8,10 +8,13 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Niramit:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
+    <!-- Header : Celia et Noussaiba-->
     <header class="header-accueil">
         <div class="triangle-down"></div>
+        <!-- Nav desktop -->
         <nav class="nav-desktop">
             <ul>
                 <li><a href="Home.html">Accueil</a></li>
@@ -25,7 +28,88 @@
                 <li><a href="Contact.html">Contact</a></li>
             </ul>
         </nav>
-        <img class="logo-nav" src="assets/img/logo-couleur.png" alt="Logo de l'association Kapital Jeunesse">
+        <div class="logo-accueil">
+            <img src="assets/img/logo-couleur.png" alt="Logo de l'association Kapital Jeunesse">
+        </div>
+
+        <div class="home-cards">
+            <div>
+                <img src="assets/img/actions.png" alt="">
+                <h3>Nos actions</h3>
+            </div>
+            <div>
+                <img src="assets/img/dons.png" alt="">
+                <h3>Faire un don</h3>
+            </div>
+            <div>
+                <img src="assets/img/location.png" alt="">
+                <h3>locations</h3>
+            </div>
+        </div>
+        
   </header>
+
+
+    <!-- Main : Celia-->
+  <main>
+        <div class="container">
+            <h1>Bienvenue sur <span id="kapital">KAPITAL JEUNESSE</span></h1>
+            <p id="texte-intro">Un espace dédié à l’essor et à l’épanouissement des jeunes, où les opportunités se déploient et les rêves prennent forme. Notre association s’engage à accompagner chaque jeune dans son parcours vers la réussite, en offrant un environnement propice à l’apprentissage, à la créativité et à l’innovation. À travers nos programmes variés et nos initiatives dynamiques, nous visons à nourrir les ambitions, à cultiver les talents et à favoriser le développement personnel de chacun. Rejoignez-nous dans cette aventure passionnante où le potentiel des jeunes est au cœur de notre action, pour construire ensemble un avenir prometteur et inspirant.</p>
+            <button class="btn-accueil">A propos de nous</button>
+        </div>
+    </main>
+    <section class="section-accueil">
+        <img class="img-accueil" src="img/billy.png" alt="image d'une action de kapital jeunesse">
+        <div class="texte-btn">
+            <p class="texte-accueil">Explorez nos activités variées, allant des événements inspirants aux initiatives innovantes, conçues pour nourrir le potentiel des jeunes et les aider à s’épanouir pleinement.</p>
+            <button class="btn-accueil">Nos actions</button>
+        </div>
+    </section>
+    <section class="section-accueil">
+        <div class="texte-btn">
+            <p class="texte-accueil">Découvrez comment vous pouvez contribuer à notre cause en faisant un don. Chaque contribution compte pour soutenir nos actions en faveur de l’épanouissement des jeunes et de leur développement personnel.</p>
+            <button class="btn-accueil">Don</button>
+        </div>
+        <img class="img-accueil" src="img/billy.png" alt="image d'un don de kapital jeunesse">
+    </section>
+    <section class="section-accueil">
+        <div class="container">
+            <img class="img-accueil" src="img/billy.png" alt="image d'un don de kapital jeunesse">
+            <p class="texte-accueil">Découvrez nos services de location d’espaces adaptés à vos besoins professionnels. Que vous organisiez des conférences, des séminaires, des formations ou des réunions d’équipe, nos locaux offrent un cadre professionnel et fonctionnel pour garantir le succès de votre événement. Bénéficiez d’installations modernes, équipées des dernières technologies, ainsi que d’un service client dédié pour répondre à vos exigences. </p>
+            <button class="btn-accueil">Location</button>
+        </div>
+    </section>
+
+    <!-- Footer : Noussaiba-->
+
+    <footer>
+        <div class="footer-container">
+            
+            <div class="footer-links">
+                <ul>
+                    <li><a href="Home.html">Accueil</a></li>
+                    <li><a href="About.html">A propos</a></li>
+                    <li><a href="Programs.html">Nos actions</a></li>
+                    <li><a href="Donate.html">Dons</a></li>
+                    <li><a href="Location.html">Localisation</a></li>
+                    <li><a href="Contact.html">Contact</a></li>
+                </ul>
+            </div>
+            <div class="footer-logo">
+                <img src="assets/img/logo-couleur.png" alt="Logo de l'association Kapital Jeunesse">
+            </div>
+            <div class="footer-social">
+                <ul>
+                    <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-x-twitter"></i></a></li>
+                    
+                </ul>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>&copy; 2021 Kapital Jeunesse. Tous droits réservés.</p>
+        </div>
+    </footer>
 </body>
 </html>
